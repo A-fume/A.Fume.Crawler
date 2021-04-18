887263eb8f6e7e016fb321a2c88e469d2fa51ac3
Heesung Youn <heesung6701@naver.com>
2021-04-18 09:27:13 +0900

    Update IngredientCrawler

--- src/IngredientCrawler.py
+++ src/IngredientCrawler.py
@@ -6,91 +6,74 @@ import time
 import random
 import os
 import csv
+import json
 from datetime import datetime
-from dotenv import load_dotenv
-import Repository
-from Repository import get_ingredient_idx
-from Repository import get_series_idx
-from Repository import set_header
+
+# 현재 수행중인 코드가 담긴 파일의 디렉토리 절대 경로를 얻는다.
+from src.Repository import set_header
+from src.CommonCrawler import common_crawler
+from src.Repository import get_series_idx, get_ingredient_idx
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-# 재료 링크 리스트 가져오기
-def ingredient_list_crawler(dir_path, site_url, query_ingredient, query_ingredient_name, query_ingredient_series_name,
-                            query_ingredient_descripyion,
-                            header):
+# 재료 리스트 가져오기
+def ingredient_list_crawler(dir_path, headers):
+    with open(os.path.join(BASE_DIR, "../json/ingredient_list.json")) as json_file:
+        json_ingredient_list = json.load(json_file)
+
+    with open(os.path.join(BASE_DIR, "../json/ingredient.json")) as json_file:
+        json_ingredient = json.load(json_file)
+
+    result_link = common_crawler(json_ingredient_list, headers)
+
+    ingredient_list = []
+    for href in map(lambda x: x["href"], result_link["link"]):
+        result = common_crawler(json_ingredient, headers, base_url=href)
+
+        ingredient = {"ingredientName": result["name"][0].text, "seriesName": result["seriesName"][0].text}
+        ingredient["seriesIdx"] = get_series_idx(ingredient["seriesName"])
+        ingredient["ingredientIdx"] = get_ingredient_idx(ingredient["ingredientName"], ingredient["description"],
+                                                         ingredient["seriesIdx"])
+        if len(result["description"]) > 0:
+            ingredient["description"] = result["description"][0].text.replace('Odor profile: ', '')
+        else:
+            ingredient["description"] = ''
+
+        ingredient_list.append(ingredient)
+        print(str(ingredient))
+
     try:
-        res = requests.get(site_url, headers={'User-agent': header})  # 헤더 추가해서 429 에러 우회
-        bs = BeautifulSoup(res.text, 'html.parser')
-
         # csv 파일 생성
         file = open(dir_path + "ingredients.csv", mode="w", encoding="utf-8", newline="")
         writer = csv.writer(file)
-        writer.writerow(["ingredientIdx", "name", "englishName", "description", "imageUrl", "seriesIdx"])
+        columns = ["ingredientIdx", "name", "englishName", "description", "imageUrl", "seriesIdx"]
+        writer.writerow(columns)
 
-        # 1. 재료 목록 가져오기
-        all_ingredient_url = []
-        for i in bs.select(query_ingredient):
-            ingredient_url = i["href"]
-            all_ingredient_url.append(ingredient_url)
-        print(all_ingredient_url)
-
-        # 2. 특정 재료 정보 가져오기
-        for ingredient_url in all_ingredient_url:
-            time.sleep(random.randrange(3, 6))
-            print(ingredient_url)
-
-            res2 = requests.get(ingredient_url, headers={'User-agent': header})  # 헤더 추가해서 429 에러 우회
-            bs2 = BeautifulSoup(res2.text, 'html.parser')
-            ingredient_info = {}
-
-            # body 내부 main content 발췌
-            main_content = bs2.select_one('#main-content')
-
-            # body 내부 main content 발췌
-            ingredient_name = main_content.select_one(query_ingredient_name).text
-            print(ingredient_name)
-
-            series_name = main_content.select_one(query_ingredient_series_name).text
-            print(series_name)
-            series_idx = get_series_idx(series_name)
-            print(series_idx)
-
-            div_ingredient = main_content.select_one(query_ingredient_descripyion)
-
-            ingredient_description = ''
-            if div_ingredient is not None:
-                ingredient_description = div_ingredient.text.replace('Odor profile: ', '')
-            print(ingredient_description)
-
-            ingredient_idx = get_ingredient_idx(ingredient_name, ingredient_description, series_idx)
-
-            print(ingredient_name + "/" + ingredient_description + "/" + str(series_idx))
-            writer.writerow([ingredient_idx, ingredient_name, ingredient_name, ingredient_description, "", series_idx])
-
+        for ingredient in ingredient_list:
+            row = []
+            for column in columns:
+                row.append(ingredient[column] or '')
+            writer.writerow(row)
         file.close()
-
     except AttributeError as e:
         print(e)
-
-    return all_ingredient_url
+    return ingredient_list
 
 
 if __name__ == '__main__':
-    # .env 파일을 절대 경로로 불러오기. 
+    from dotenv import load_dotenv
+
+    # .env 파일을 절대 경로로 불러오기.
     # (python 2.8부터는 상대 경로로도 가능한 것으로 보인다.)
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__)) # 현재 수행중인 코드가 담긴 파일의 디렉토리 절대 경로를 얻는다.
-    load_dotenv(dotenv_path=os.path.join(BASE_DIR, "../.env")) # .env 파일의 상대경로와 현재 디렉토리의 절대 경로를 조합해서, .env파일을 절대 경로로 불러온다.
-    
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # 현재 수행중인 코드가 담긴 파일의 디렉토리 절대 경로를 얻는다.
+    load_dotenv(
+        dotenv_path=os.path.join(BASE_DIR, "../.env"))  # .env 파일의 상대경로와 현재 디렉토리의 절대 경로를 조합해서, .env파일을 절대 경로로 불러온다.
+
     email = os.getenv('ADMIN_EMAIL')
     password = os.getenv('ADMIN_PWD')
-    site_url_ingredient = os.getenv('SITE_URL_INGREDIENT')
     set_header(email, password)
 
-    site_url_ingredient = os.getenv('SITE_URL_INGREDIENT')
-    query_ingredient = os.getenv('QUERY_INGREDIENT_HREF')
-    query_ingredient_name = os.getenv('QUERY_INGREDIENT_NAME')
-    query_ingredient_series_name = os.getenv('QUERY_INGREDIENT_SERIES_NAME')
-    query_ingredient_description = os.getenv('QUERY_INGREDIENT_DESCRIPTION')
     header = os.getenv('HEADER')
 
     folder_name = datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
@@ -98,8 +81,4 @@ if __name__ == '__main__':
     os.makedirs(path)
     print('start test')
     print('generated file: ' + folder_name)
-    ingredient_list_crawler(dir_path=path, site_url=site_url_ingredient, query_ingredient=query_ingredient,
-                            query_ingredient_name=query_ingredient_name,
-                            query_ingredient_series_name=query_ingredient_series_name,
-                            query_ingredient_descripyion=query_ingredient_description,
-                            header=header)
+    ingredient_list_crawler(dir_path=path, headers={'User-agent': header})

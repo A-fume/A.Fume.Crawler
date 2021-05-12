c683c48fa2fb88fdd3df2bd7c6efea8f39917203
윤희성 <heesung6701@naver.com>
2021-05-12 20:28:17 +0900

    Add perfume_create

--- src/repository/PerfumeRepository.py
+++ src/repository/PerfumeRepository.py
@@ -1,13 +1,29 @@
 import requests
-from Auth import Auth
+from src.repository.Auth import Auth
 
 
-def get_perfume_idx(english_name):
+def create_perfume(english_name, brand_idx, image_url, story):
+    name = english_name
+    abundance_rate = "None"
+    release_date = "2021-05-09"
+    volume_and_price = []
+    print({"name": name, "englishName": english_name, "abundanceRate": abundance_rate,
+           "volumeAndPrice": volume_and_price,
+           "imageUrl": image_url, "brandIdx": brand_idx, "releaseDate": release_date,
+           "story": story})
+    response = Auth.instance().request(requests.post, url='/perfume',
+                                       json={"name": name, "englishName": english_name, "abundanceRate": abundance_rate,
+                                             "volumeAndPrice": volume_and_price,
+                                             "imageUrl": image_url, "brandIdx": brand_idx, "releaseDate": release_date,
+                                             "story": story})
+    print(response.text)
+    return response.json()['data']
+
+
+def get_perfume_idx(english_name, brand_idx, image_url, story):
     response = Auth.instance().request(requests.post, url='/getPerfumeIdx', json={"englishName": english_name})
     if response.status_code / 100 > 2:
-        print(response.status_code)
-        print(response.text)
-        return -1
+        return create_perfume(english_name, brand_idx, image_url, story)
     return response.json()['data']
 
 
@@ -18,7 +34,7 @@ if __name__ == '__main__':
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     load_dotenv(dotenv_path=os.path.join(BASE_DIR, '../../.env'), verbose=True)
 
-    if get_perfume_idx('154 Cologne') == 1:
+    if get_perfume_idx('154 Cologne2', 1, "", "test story") == 1:
         print('success getPerfumeIdx()')
     else:
         print('failed getPerfumeIdx()')
